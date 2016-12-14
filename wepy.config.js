f98b9155bc13631acda956cf4150f290f5e6896d
changong(龚澄) <changong@tencent.com>
2016-12-14 18:50:52 +0800

    first init

---
+++ wepy.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    "wpyExt": ".wpy",
+    "src": "",
+    "dist": "",
+    "babel": {
+        "presets": [
+            "es2015",
+            "stage-1"
+        ],
+        "plugins": [
+            "transform-export-extensions",
+            "syntax-export-extensions",
+            "transform-runtime"
+        ]
+    }
+};
+
